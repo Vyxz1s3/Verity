6ecd71d4bde018062dd69b11d369faef14d65d0f
Vyxz1s3 <clymansjerni@gmail.com>
2026-06-15 14:25:36 +0200

    Update promote.js

--- src/commands/Moderation/promote.js
+++ src/commands/Moderation/promote.js
@@ -1,1 +1,79 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
+export default {
+    data: new SlashCommandBuilder()
+        .setName('promote')
+        .setDescription('Create a promotion record')
+        .addUserOption(option =>
+            option
+                .setName('target')
+                .setDescription('User being promoted')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('old_rank')
+                .setDescription('Current rank')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('new_rank')
+                .setDescription('New rank')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('reason')
+                .setDescription('Reason for promotion')
+                .setRequired(true)
+        ),
+
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+
+    category: 'promotions',
+
+    async execute(interaction) {
+        const user = interaction.options.getUser('target');
+        const oldRank = interaction.options.getString('old_rank');
+        const newRank = interaction.options.getString('new_rank');
+        const reason = interaction.options.getString('reason');
+
+        const promotionEmbed = {
+            title: '📈 Promotion Record',
+            color: 0x00ff00,
+            fields: [
+                {
+                    name: 'Promoted User',
+                    value: `<@${user.id}>`,
+                    inline: true,
+                },
+                {
+                    name: 'Previous Rank',
+                    value: oldRank,
+                    inline: true,
+                },
+                {
+                    name: 'New Rank',
+                    value: newRank,
+                    inline: true,
+                },
+                {
+                    name: 'Promoted By',
+                    value: `<@${interaction.user.id}>`,
+                    inline: true,
+                },
+                {
+                    name: 'Reason',
+                    value: reason,
+                    inline: false,
+                },
+            ],
+            timestamp: new Date(),
+        };
+
+        await interaction.reply({
+            embeds: [promotionEmbed],
+        });
+    },
+};
