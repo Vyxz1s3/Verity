856fa0ba01406154447552684386f8e920cfe730
Vyxz1s3 <clymansjerni@gmail.com>
2026-06-17 21:27:04 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -12,7 +12,7 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "idle",
+    status: "online",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Created By Atlas Design!",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 2, 
       },
     ],
   },
